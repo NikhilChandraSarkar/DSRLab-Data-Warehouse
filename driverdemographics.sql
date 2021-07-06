48d1655059009a754df5ed0ddc629bd1e3dc1b97
Nikhil Chandra Sarkar <34510262+NikhilChandraSarkar@users.noreply.github.com>
2021-07-07 08:28:56 +1000

    Add files via upload

---
+++ driverdemographics.sql
@@ -0,0 +1,18 @@
+CREATE TABLE [driverdemographics] (
+    [Crash_Year] varchar(50),
+    [Crash_Police_Region] varchar(50),
+    [Crash_Severity] varchar(50),
+    [Involving_Male_Driver] varchar(50),
+    [Involving_Female_Driver] varchar(50),
+    [Involving_Young_Driver_16-24] varchar(50),
+    [Involving_Senior_Driver_60plus] varchar(50),
+    [Involving_Provisional_Driver] varchar(50),
+    [Involving_Overseas_Licensed_Driver] varchar(50),
+    [Involving_Unlicensed_Driver] varchar(50),
+    [Count_Crashes] varchar(50),
+    [Count_Casualty_Fatality] varchar(50),
+    [Count_Casualty_Hospitalised] varchar(50),
+    [Count_Casualty_MedicallyTreated] varchar(50),
+    [Count_Casualty_MinorInjury] varchar(50),
+    [Count_Casualty_All] varchar(50)
+)
