c5f3c8783ae0830e53986d301e0dd68e40f5d704
Nikhil Chandra Sarkar <34510262+NikhilChandraSarkar@users.noreply.github.com>
2021-07-07 08:30:12 +1000

    Add files via upload

---
+++ roadcasualties.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [roadcasualties] (
+    [Crash_Year] varchar(50),
+    [Crash_Police_Region] varchar(50),
+    [Casualty_Severity] varchar(50),
+    [Casualty_AgeGroup] varchar(50),
+    [Casualty_Gender] varchar(50),
+    [Casualty_RoadUserType] varchar(50),
+    [Casualty_Count] varchar(50)
+)
