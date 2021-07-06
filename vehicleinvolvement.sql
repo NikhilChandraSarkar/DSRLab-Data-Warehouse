46352dc9bfa5fbe6781c5fca79c3f842281bc6ee
Nikhil Chandra Sarkar <34510262+NikhilChandraSarkar@users.noreply.github.com>
2021-07-07 08:30:46 +1000

    Add files via upload

---
+++ vehicleinvolvement.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [vehicleinvolvement] (
+    [Crash_Year] varchar(50),
+    [Crash_Police_Region] varchar(50),
+    [Crash_Severity] varchar(50),
+    [Count_Crashes] varchar(50),
+    [Involving_Motorcycle_Moped] varchar(50),
+    [Involving_Truck] varchar(50),
+    [Involving_Bus] varchar(50),
+    [Count_Casualty_Fatality] varchar(50),
+    [Count_Casualty_Hospitalised] varchar(50),
+    [Count_Casualty_MedicallyTreated] varchar(50),
+    [Count_Casualty_MinorInjury] varchar(50),
+    [Count_Casualty_All] varchar(50)
+)
