622031a166d942066069672c264e75898ba0da4d
Nikhil Chandra Sarkar <34510262+NikhilChandraSarkar@users.noreply.github.com>
2021-07-07 08:29:52 +1000

    Add files via upload

---
+++ restrainthelmetuse.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [restrainthelmetuse] (
+    [Crash_Year] varchar(50),
+    [Crash_PoliceRegion] varchar(50),
+    [Casualty_Severity] varchar(50),
+    [Casualty_AgeGroup] varchar(50),
+    [Casualty_Gender] varchar(50),
+    [Casualty_Road_User_Type] varchar(50),
+    [Casualty_Restraint_Helmet_Use] varchar(50),
+    [Casualty_Count] varchar(50)
+)
