0411e9e32331c2f04af455e257a099799ca0d58f
Nikhil Chandra Sarkar <34510262+NikhilChandraSarkar@users.noreply.github.com>
2021-07-07 08:29:24 +1000

    Add files via upload

---
+++ factorsinroadcrashes.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [factorsinroadcrashes] (
+    [Crash_Year] varchar(50),
+    [Crash_Police_Region] varchar(50),
+    [Crash_Severity] varchar(50),
+    [Count_Crashes] varchar(50),
+    [Involving_Drink_Driving] varchar(50),
+    [Involving_Driver_Speed] varchar(50),
+    [Involving_Fatigued_Driver] varchar(50),
+    [Involving_Defective_Vehicle] varchar(50),
+    [Count_Fatality] varchar(50),
+    [Count_Hospitalised] varchar(50),
+    [Count_Medically_Treated] varchar(50),
+    [Count_Minor_Injury] varchar(50),
+    [Count_All_Casualties] varchar(50)
+)
